50b94c3673085b83711cecd2434ea1d518bc2403
Daniel Felipe <piperruno22@gmail.com>
2015-09-24 17:09:00 -0500

    Create check-windows-cpu-load.rb

---
+++ check-windows-cpu-load.rb
@@ -0,0 +1,50 @@
+#! /usr/bin/env ruby
+#
+#   check-windows-cpu-load.rb modificado por daniel felipe morales para windows server en español
+#
+# DESCRIPTION:
+#
+# OUTPUT:
+#   plain text
+#
+# PLATFORMS:
+#   Windows
+#
+# DEPENDENCIES:
+#   gem: sensu-plugin
+#
+# USAGE:
+#
+# NOTES:
+#  Tested on Windows 2008RC2.
+#
+# LICENSE:
+#   Jean-Francois Theroux <me@failshell.io>
+#   Released under the same terms as Sensu (the MIT license); see LICENSE
+#   for details.
+#
+
+require 'sensu-plugin/check/cli'
+
+#
+# Check Windows CPU Load
+#
+class CheckWindowsCpuLoad < Sensu::Plugin::Check::CLI
+  option :warning,
+         short: '-w WARNING',
+         default: 85,
+         proc: proc(&:to_i)
+
+  option :critical,
+         short: '-c CRITICAL',
+         default: 95,
+         proc: proc(&:to_i)
+
+  def run
+    io = IO.popen("typeperf -sc 1 \"Procesador(_Total)\\% de tiempo de procesador\"")
+    cpu_load = io.readlines[2].split(',')[1].gsub(/"/, '').to_i
+    critical "CPU at #{cpu_load}%" if cpu_load > config[:critical]
+    warning "CPU at #{cpu_load}%" if cpu_load > config[:warning]
+    ok "CPU at #{cpu_load}%"
+  end
+end
